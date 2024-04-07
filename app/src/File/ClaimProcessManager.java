8b8515b13f388f21cba58baaea6b9ce62338de0e
TruongThinh14 <95459900+TruongThinh14@users.noreply.github.com>
2024-04-07 17:33:11 +0700

    update class to manage claims

--- app/src/File/ClaimProcessManager.java
+++ app/src/File/ClaimProcessManager.java
@@ -9,3 +9,38 @@ interface ClaimProcessManager {
     Claim getOne(String claimId);
     List<Claim> getAll();
 }
+
+// Class to manage claims
+class ClaimManager implements ClaimProcessManager {
+    private List<Claim> claimList;
+
+    ClaimManager(List<Claim> initialClaims) {
+        this.claimList = initialClaims;
+    }
+
+    @Override
+    public void add(Claim claim) {
+        claimList.add(claim);
+    }
+
+    @Override
+    public void update(Claim claim) {
+        // Update logic
+    }
+
+    @Override
+    public void delete(Claim claim) {
+        claimList.remove(claim);
+    }
+
+    @Override
+    public Claim getOne(String claimId) {
+        // Find and return claim by ID
+        return null;
+    }
+
+    @Override
+    public List<Claim> getAll() {
+        return claimList;
+    }
+}
--- app/src/File/PolicyHolder.java
+++ app/src/File/PolicyHolder.java
@@ -1,4 +1,17 @@
 package File;
 
-public class PolicyHolder {
+import java.util.List;
+
+public class PolicyHolder extends Customer {
+    private List<Dependent> dependents;
+
+    public void addDependent(Dependent dependent) {
+        dependents.add(dependent);
+    }
+
+    public void removeDependent(Dependent dependent) {
+        dependents.remove(dependent);
+    }
+
+    // Constructor, getters, and setters
 }
