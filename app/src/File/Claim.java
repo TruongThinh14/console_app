735760dee2a11a2db2f2f8c8bf75d6190c81f41e
TruongThinh14 <95459900+TruongThinh14@users.noreply.github.com>
2024-04-07 17:26:21 +0700

    update Claim class

--- app/src/File/Claim.java
+++ app/src/File/Claim.java
@@ -1,4 +1,19 @@
 package File;
 
+import java.util.Date;
+import java.util.List;
+
 public class Claim {
+    private String id;
+    private Date claimDate;
+    private String insuredPerson;
+    private String cardNumber;
+    private Date examDate;
+    private List<String> documents; // List of document names
+    private double claimAmount;
+    private String status;
+    private String receiverBankName;
+    private String receiverBankNumber;
+
+    // Constructor, getters, and setters
 }
